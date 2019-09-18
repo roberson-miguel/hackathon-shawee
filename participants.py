46f92b3857175b98398b4188795d1edb953e2ba5
Luke <lukells@outlook.com>
2019-09-17 21:40:58 -0300

    initial commit

---
+++ participants.py
@@ -0,0 +1,9 @@
+class Participants:
+    def __init__(self, name, role, level):
+        self.name = name
+        self.role = role
+        self.level = level
+
+    def info(self):
+        print("Name: " + self.name + " Role: " + int(self.role) + " Level: " + int(self.level))
+
