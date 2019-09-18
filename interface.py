5f307fe82558dfe3361982bdffddc9b3d7cb591c
Luke <lukells@outlook.com>
2019-09-17 22:24:13 -0300

    modified file to use functions

---
+++ interface.py
@@ -0,0 +1,39 @@
+from participant import Participant
+
+participantsList = []
+registerAnother = 's'
+confirm = 'n'
+
+def getUserData():
+    global name
+    global role
+    global level
+    name = input("Digite o seu nome: ")
+    print("Considerando os seguintes papéis: ")
+    print("1 - Desenvolvedor")
+    print("2 - Negócios")
+    print("3 - UX/UI")
+    print("4 - PO")
+    role = int(input("Digite o papel desejado: "))
+    level = int(input("Numa escala de 0 a 10, qual o seu nível de conhecimento nesse papel? ")) 
+   
+def registerUser():
+    global confirm
+    global registerAnother
+    confirm = input("Papel: " + str(role) + " Nível: " + str(level) + ". Podemos confirmar sua inscrição ? (s/n) ")
+    if confirm == "S" or confirm == "s": 
+        participantsList.append(Participant(name, role, level))
+        print("Inscrição confirmada com sucesso!")
+        for participant in participantsList:
+            print(participant.name)
+       
+    else: 
+        print("Inscrição cancelada!")    
+        registerAnother = input("Deseja registrar outro usuário? (s/n)")
+
+def run():
+    while registerAnother == 's' or registerAnother == 'S':
+        getUserData()
+        registerUser()
+
+run()
