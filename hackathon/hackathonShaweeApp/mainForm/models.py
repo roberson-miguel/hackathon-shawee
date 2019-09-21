21aec7aa6c90447c672552f42d9f764154a3a888
Luke <lukells@outlook.com>
2019-09-20 23:05:00 -0300

    created initial user model

--- hackathon/hackathonShaweeApp/mainForm/models.py
+++ hackathon/hackathonShaweeApp/mainForm/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model): 
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    age = models.IntegerField(max_length=3)
+    specialization = models.CharField(max_length=30)
