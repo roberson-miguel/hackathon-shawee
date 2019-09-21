66b29cbd2c93ab122918af96e569c6d51593a8ff
Luke <lukells@outlook.com>
2019-09-21 00:15:14 -0300

    added data save logic

--- hackathon/hackathonShaweeApp/mainForm/models.py
+++ hackathon/hackathonShaweeApp/mainForm/models.py
@@ -4,5 +4,5 @@ from django.utils import timezone
 class Participant(models.Model): 
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    age = models.IntegerField(max_length=3)
+    age = models.IntegerField()
     specialization = models.CharField(max_length=30)
