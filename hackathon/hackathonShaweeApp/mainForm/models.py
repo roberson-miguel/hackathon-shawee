83275e71a337195b64d9e5dd368a0785aa577365
Luke <lukells@outlook.com>
2019-09-20 23:05:30 -0300

    changed class name from user to participant

--- hackathon/hackathonShaweeApp/mainForm/models.py
+++ hackathon/hackathonShaweeApp/mainForm/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-class User(models.Model): 
+class Participant(models.Model): 
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     age = models.IntegerField(max_length=3)
